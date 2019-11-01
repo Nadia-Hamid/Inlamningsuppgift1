a88927b25cde1cb54d0e13a40bbc41510002218e
Nadia <hamid.nadia@gmail.com>
2019-11-01 14:56:04 +0100

    Adding user access

--- GuessingGame.java
+++ GuessingGame.java
@@ -1,8 +1,12 @@
+
 public class GuessingGame {
 
 	public static void main(String[] args) {
-	//
-	Guesser guess = new Guesser(0,1000);
+	
+
+	
+	Guesser guess = new Guesser(Integer.parseInt(args[0]),Integer.parseInt(args[1]));
 	guess.start();
 	}
   }
+
